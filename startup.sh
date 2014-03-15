de298ae387838766d2070f565f82ba73d3c2deed
Tom Fotherby <tom+github@peopleperhour.com>
2014-03-15 00:16:07 +0000

    Find env vars dynamically instead of hardcoding

--- startup.sh
+++ startup.sh
@@ -1,36 +1,29 @@
  #!/bin/sh
 
-# update the fpm configuration to make the service environment variables available
+# Function to update the fpm configuration to make the service environment variables available
 function setEnvironmentVariable() {
 
-        if [ -z "$2" ]; then
-                echo "Environment variable $1 not set."
-                return
-        fi  
+    if [ -z "$2" ]; then
+            echo "Environment variable '$1' not set."
+            return
+    fi
 
-        # This is a weird hack, because the php-fpm environment does not see the $MYSQL_PORT_3306_TCP_ADDR variable...
-        if grep -q MYSQL_HOST /etc/php5/fpm/pool.d/www.conf; then
-                sed -i "s/^env\[$1.*/env[$1] = $2/g" /etc/php5/fpm/pool.d/www.conf
-        else
-                echo "env[$1] = $2" >> /etc/php5/fpm/pool.d/www.conf
-        fi  
+    # Check whether variable already exists
+    if grep -q $1 /etc/php5/fpm/pool.d/www.conf; then
+        # Reset variable
+        sed -i "s/^env\[$1.*/env[$1] = $2/g" /etc/php5/fpm/pool.d/www.conf
+    else
+        # Add variable
+        echo "env[$1] = $2" >> /etc/php5/fpm/pool.d/www.conf
+    fi
 }
 
-# set environment variables to other services
-setEnvironmentVariable MONGO_PORT_27017_TCP_ADDR $MONGO_PORT_27017_TCP_ADDR
-setEnvironmentVariable MONGO_PORT_27017_TCP_PORT $MONGO_PORT_27017_TCP_PORT
-
-setEnvironmentVariable MYSQL_PORT_3306_TCP_ADDR $MYSQL_PORT_3306_TCP_ADDR
-setEnvironmentVariable MYSQL_PORT_3306_TCP_PORT $MYSQL_PORT_3306_TCP_PORT
-
-setEnvironmentVariable REDIS_PORT_6379_TCP_ADDR $REDIS_PORT_6379_TCP_ADDR
-setEnvironmentVariable REDIS_PORT_6379_TCP_PORT $REDIS_PORT_6379_TCP_PORT
-
-setEnvironmentVariable ELASTIC_PORT_9200_TCP_ADDR $ELASTIC_PORT_9200_TCP_ADDR
-setEnvironmentVariable ELASTIC_PORT_9200_TCP_PORT $ELASTIC_PORT_9200_TCP_PORT
-
-setEnvironmentVariable RABBIT_PORT_5672_TCP_ADDR $RABBIT_PORT_5672_TCP_ADDR
-setEnvironmentVariable RABBIT_PORT_5672_TCP_PORT $RABBIT_PORT_5672_TCP_PORT
+# Grep for variables that look like docker set them (_PORT_)
+for _curVar in `env | grep _PORT_ | awk -F = '{print $1}'`;do
+    # awk has split them by the equals sign
+    # Pass the name and value to our function
+    setEnvironmentVariable ${_curVar} ${!_curVar}
+done
 
  # start php-fpm
 service php5-fpm start
